75c22c296f06d5872ffb916a6f30dbe8676f0dcc
Pedro_Hnrk <lpeterz94@outlook.com>
2025-05-16 01:17:27 -0300

    Comentando codigo

--- codigo/main.c
+++ codigo/main.c
@@ -3,60 +3,73 @@
 #include <string.h>
 #include <stdbool.h>
 
+// struct Contato
+// armazena os dados do contato
 struct Contato {
-    int id;
-    char nome[50];
-    char telefone[11];
-    char email[100];
+    int id; // id do contato
+    char nome[50]; // nome do contato
+    char telefone[11]; // telefone do contato
+    char email[100]; // email do contato
 };
 
+// função authEmail
+// verifica se o email é válido
 bool authEmail(char email[]) {
-    char *p = strchr(email, '@');
-    if (p != NULL) {
+    char *p = strchr(email, '@'); // verifica se existe o @
+    if (p != NULL) { 
         printf("Email válido\n");
-        return true;
+        return true; // retorna verdadeiro se o email for válido
     } else {
         printf("Email inválido\n");
-        return false;
+        return false; // retorna falso se o email for inválido
     }
 }
 
-void preenche(struct Contato contatos[], int id) {
-    for (int i = id; i == id; i++) {
-        contatos[i].id = i * 1.00;
-        printf("digite o telefone (31999999999): ");
-        scanf("%s", &contatos[i].telefone);
+// procedimento preenche
+// preenche os dados do contato
+// recebe como parâmetro o vetor de contatos e o contador de contatos
+void preenche(struct Contato contatos[], int cont) {
+    for (int i = cont; i == cont; i++) {
+        contatos[i].id = i + 1; // atribui o id do contato
+        printf("digite o telefone (31999999999): "); 
+        scanf("%s", &contatos[i].telefone); // atribui o telefone do contato
         fflush(stdin);
-        printf("digite o nome do contato:");
-        scanf("%s", &contatos[i].nome);
+        printf("digite o nome do contato:"); 
+        scanf("%s", &contatos[i].nome); // atribui o nome do contato
         fflush(stdin);
-        do {
-        printf("digite o email do contato:");
-        scanf("%s", &contatos[i].email);
+        do { // em loop até o email ser válido
+        printf("digite o email do contato:"); 
+        scanf("%s", &contatos[i].email); // atribui o email do contato
         fflush(stdin);
         }while (!authEmail(contatos[i].email));
 
-        printf("Contato %d adicionado com sucesso!\n", contatos[i].id + 1);
+        printf("Contato %d adicionado com sucesso!\n", contatos[i].id); // imprime mensagem de sucesso
     }
 }
 
-void listar(struct Contato contatos[], int id) {
-    printf("Lista de contatos:\n");
-    for (int i = 0; i < id; i++) {
+// procedimento listar
+// imprime a lista de contatos
+void listar(struct Contato contatos[], int cont) {
+    printf("Lista de contatos:\n"); 
+    for (int i = 0; i < cont; i++) {
+        // TODO: criar procedimento para imprimir dados do contato
         printf("\nID: %d\n", contatos[i].id);
         printf("Nome: %s\n", contatos[i].nome);
-        printf("Telefone: %s\n", contatos[i].telefone);
+        printf("Telefone: %s\n", contatos[i].telefone); //TODO: transformar o telefone para o formato (31) 99999-9999
         printf("Email: %s\n", contatos[i].email);
         printf("------------------------\n");
     }
 }
 
 int main() {
-    int max_len = 10;
-    int op = 0;
-    int id = 0;
+    int max_len = 10; // tamanho máximo do vetor de contatos
+    int op = 0; // variável para armazenar a opção do usuário
+    int cont = 0; // contador de contatos
+    // aloca memória para o vetor de contatos
     struct Contato *contatos = (struct Contato*)malloc(max_len * sizeof(struct Contato));
-    do {
+    
+    
+    do { // loop até o usuário digitar -1
         printf("\tMenu:\t\n1 - Adicionar contato\n");
         printf("2 - Listar contatos\n");
         printf("digite a opção: ");
@@ -66,14 +79,13 @@ int main() {
 
         switch (op)
         {
-        case 1:
-            preenche(contatos, id);
-            id++;
+        case 1: // adiciona contato
+            preenche(contatos, cont);
+            cont++; // incrementa o contador de contatos
             break;
         
-        case 2:
-            listar(contatos, id);
-            
+        case 2: // lista contatos
+            listar(contatos, cont);
             break;
         
         default:
@@ -82,7 +94,8 @@ int main() {
         }
     }while (op != -1);
         
-    
+    free(contatos); // libera a memória alocada para o vetor de contatos
+    contatos = NULL; // zera o ponteiro para evitar acesso a memória liberada
     
 }
     
